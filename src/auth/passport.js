b6d83a12165a2d874c27bd8d448feb7e237da219
sugoides <john.neri13@gmail.com>
2025-12-07 04:37:47 +0800

    update environment variables in .sample-env and docker-compose.yml; add BASE_URL and fix indentation in passport.js

--- src/auth/passport.js
+++ src/auth/passport.js
@@ -8,7 +8,8 @@ passport.deserializeUser((obj, done) => done(null, obj));
 passport.use(new SteamStrategy({
   returnURL: `${process.env.BASE_URL}/auth/steam/return`,
   realm: process.env.BASE_URL,
-  apiKey: process.env.STEAM_API_KEY
+  apiKey: process.env.STEAM_API_KEY,
+  profile: true
 }, (identifier, profile, done) => {
   // profile.id is steamid
   process.nextTick(() => done(null, profile));
